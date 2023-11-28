01a4ea2ede07d5bc9ebd6707cac0d114d44bd4a7
Rajat Mehta <rjtmehta99@gmail.com>
2023-11-28 16:13:08 +0100

    added multiple urls

--- scrape_smart_recruiter.py
+++ scrape_smart_recruiter.py
@@ -1,36 +1,38 @@
 # TODO: parameterize location and company names.
 # Apply via LinkedIn automatically, stop at other questions.
+from __future__ import annotations
 import constants
 import pandas as pd
 import selenium_helper
 
-def scrape(url: str) -> pd.DataFrame:
-    selenium = selenium_helper.Selenium(url=url, headless=True)
-    selenium.click_by_id(id_='onetrust-accept-btn-handler')
+def scrape(urls: list[str]) -> pd.DataFrame:
+    for url in urls:
+        selenium = selenium_helper.Selenium(url=url, headless=True)
+        selenium.click_by_id(id_='onetrust-accept-btn-handler')
 
-    urls = []
-    positions = []
-    companies = []
-    locations = []
+        job_urls = []
+        positions = []
+        companies = []
+        locations = []
 
-    table = selenium.element_by_class(class_='jobs-list')
-    rows = selenium.elements_by_class(class_='jobs-item', selected_element=table)
+        table = selenium.element_by_class(class_='jobs-list')
+        rows = selenium.elements_by_class(class_='jobs-item', selected_element=table)
 
-    for row in rows:
-        job_details = selenium.element_by_tag(tag='a', selected_element=row)
-        url = job_details.get_attribute('href')
-        job_details = job_details.text.split('\n')
-        position = job_details[0]
-        company = job_details[1]
-        location = job_details[2]
-        #if company.lower.__contains__('gmbh') or location.lower.__contains__('germany')
-        if 'gmbh' in company.lower() or ('germany' or 'deutschland') in location.lower():
-            urls.append(url)
-            positions.append(position)
-            companies.append(company)
-            locations.append(location)
-    print(f'{len(urls)} relevant jobs found.', end='\n-----\n')
-    selenium.quit_driver()
+        for row in rows:
+            job_details = selenium.element_by_tag(tag='a', selected_element=row)
+            job_url = job_details.get_attribute('href')
+            job_details = job_details.text.split('\n')
+            position = job_details[0]
+            company = job_details[1]
+            location = job_details[2]
+            #if company.lower.__contains__('gmbh') or location.lower.__contains__('germany')
+            if 'gmbh' in company.lower() or ('germany' or 'deutschland') in location.lower():
+                job_urls.append(job_url)
+                positions.append(position)
+                companies.append(company)
+                locations.append(location)
+        print(f'{len(job_urls)} relevant jobs found.', end='\n-----\n')
+        selenium.quit_driver()
     
     try:
         previous_df = pd.read_csv(constants.SMART_RECRUITERS_JOBS)
@@ -38,7 +40,7 @@ def scrape(url: str) -> pd.DataFrame:
         previous_df = pd.DataFrame(columns=['position', 'company', 'location', 'url', 'notified'])
 
     current_df = pd.DataFrame(data={'position': positions, 'company': companies, 
-                                    'location': locations, 'url': urls})
+                                    'location': locations, 'url': job_urls})
     current_df['notified'] = False
     merged_df = pd.concat([current_df, previous_df])
     merged_df = merged_df.drop_duplicates(subset=['url'], keep='last').reset_index(drop=True)
--- trigger_smart_recruiter_scraper.py
+++ trigger_smart_recruiter_scraper.py
@@ -3,8 +3,12 @@ import scrape_smart_recruiter
 import helper
 import constants
 
-url = 'https://jobs.smartrecruiters.com/?keyword=data%20scientist'
-jobs_df = scrape_smart_recruiter.scrape(url)
+urls = ['https://jobs.smartrecruiters.com/?keyword=data%20scientist', 
+        'https://jobs.smartrecruiters.com/?keyword=machine%20learning',
+        'https://jobs.smartrecruiters.com/?keyword=data%20science',
+        'https://jobs.smartrecruiters.com/?keyword=data%20analyst']
+
+jobs_df = scrape_smart_recruiter.scrape(urls)
 unnotified_jobs = jobs_df[jobs_df['notified'] == False]['url'].to_list()
 
 if len(unnotified_jobs) > 0:
