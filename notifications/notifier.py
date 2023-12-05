1b05b10c7e2244c1771305215b8e614d09f0e128
Rajat Mehta <rjtmehta99@gmail.com>
2023-12-05 14:03:56 +0100

    fixed toast

--- notifications/notifier.py
+++ notifications/notifier.py
@@ -3,20 +3,17 @@ from win11toast import toast
 import webbrowser
 from helpers import constants
 
-def open_urls(args: str, urls: list[str]) -> None:
+def open_urls(args: str, urls: list[str]|str) -> None:
     if type(urls) == list:
-        if len(urls) > 1:
-            for url in urls:
-                webbrowser.open(url)
-        else:
-            webbrowser(urls[0])
+        for url in urls:
+            webbrowser.open(url)
     else:
         webbrowser.open(urls)
 
 def notify_application_success(job_args, urls) -> None:
     toast(title='Application Sent Successfully!',
           body=f'{job_args["POSITION"]} at {job_args["COMPANY_NAME"]}.\nClick to view application.',
-          on_click= lambda args: open_urls(args, urls)
+          on_click= lambda args: open_urls(args, urls),
           scenario='incomingCall')
 
 def notify_application_failure(job_args) -> None:
--- scrapers/apply_smart_recruiters.py
+++ scrapers/apply_smart_recruiters.py
@@ -8,173 +8,162 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from cv_resume_generator.render_cover_letter import render_cover_letter
 
-
 with open(constants.CANDIDATE_DATA, 'r') as file:
     candidate_data = yaml.safe_load(file)
 
-URL = 'https://jobs.smartrecruiters.com/HitachiSolutions/743999948479923-machine-learning-ai-engineer-m-w-d-'
+def apply_smartr_job(url: str) -> None:
+    URL = 'https://jobs.smartrecruiters.com/HitachiSolutions/743999948479923-machine-learning-ai-engineer-m-w-d-'
+    selenium = selenium_helper.Selenium(url=URL)
 
-selenium = selenium_helper.Selenium(url=URL)
-
-# Click on apply / jetzt bewerben
-selenium.click_by_id('st-apply')
-
-# Reject Cookies
-try:
-    selenium.click_by_id(id_='onetrust-reject-all-handler')
-except NoSuchElementException:
-    pass
-
-# Upload resume, ensure absolute path
-selenium.upload_file_by_css(css_selector="input[type='file']", file_path=constants.RESUME_PATH)
-
-# Send first name
-selenium.clear_by_id(id_='first-name-input')
-selenium.send_keys_by_id(id_='first-name-input', key=candidate_data['first_name'])
-
-# Send last name
-selenium.clear_by_id(id_='last-name-input')
-selenium.send_keys_by_id(id_='last-name-input', key=candidate_data['last_name'])
-
-# Send email id
-selenium.clear_by_id(id_='email-input')
-selenium.send_keys_by_id(id_='email-input', key=candidate_data['email_id'])
-selenium.clear_by_id(id_='confirm-email-input')
-selenium.send_keys_by_id(id_='confirm-email-input', key=candidate_data['email_id'])
-
-# Send current location
-selenium.clear_by_xpath(xpath='//input[@class="sr-location-autocomplete element--input element--block"]')
-selenium.send_keys_by_xpath(xpath='//input[@class="sr-location-autocomplete element--input element--block"]',
-                            keys=[candidate_data['location'][:-1], Keys.DOWN, Keys.ENTER])
-
-# Send contact number
-selenium.clear_by_id(id_='phone-number-input')
-selenium.send_keys_by_id(id_='phone-number-input', key=candidate_data['mobile_number'])
-
-# Remove drafted information boxes for experience and education info from resume
-while True:
+    # Click on apply / jetzt bewerben
+    selenium.click_by_id('st-apply')
+    # Reject Cookies
     try:
-        selenium.click_by_xpath('//button[(@data-test="experience-cancel")]')
+        selenium.click_by_id(id_='onetrust-reject-all-handler')
     except NoSuchElementException:
-        break
+        pass
 
-# Remove experience and education info from resume
-while True:
+    # Upload resume, ensure absolute path
+    selenium.upload_file_by_css(css_selector="input[type='file']", file_path=constants.RESUME_PATH)
+
+    # Send first name
+    selenium.clear_by_id(id_='first-name-input')
+    selenium.send_keys_by_id(id_='first-name-input', key=candidate_data['first_name'])
+
+    # Send last name
+    selenium.clear_by_id(id_='last-name-input')
+    selenium.send_keys_by_id(id_='last-name-input', key=candidate_data['last_name'])
+
+    # Send email id
+    selenium.clear_by_id(id_='email-input')
+    selenium.send_keys_by_id(id_='email-input', key=candidate_data['email_id'])
+    selenium.clear_by_id(id_='confirm-email-input')
+    selenium.send_keys_by_id(id_='confirm-email-input', key=candidate_data['email_id'])
+
+    # Send current location
+    selenium.clear_by_xpath(xpath='//input[@class="sr-location-autocomplete element--input element--block"]')
+    selenium.send_keys_by_xpath(xpath='//input[@class="sr-location-autocomplete element--input element--block"]',
+                                keys=[candidate_data['location'][:-1], Keys.DOWN, Keys.ENTER])
+
+    # Send contact number
+    selenium.clear_by_id(id_='phone-number-input')
+    selenium.send_keys_by_id(id_='phone-number-input', key=candidate_data['mobile_number'])
+
+    # Remove drafted information boxes for experience and education info from resume
+    while True:
+        try:
+            selenium.click_by_xpath('//button[(@data-test="experience-cancel")]')
+        except NoSuchElementException:
+            break
+
+    # Remove experience and education info from resume
+    while True:
+        try:
+            selenium.click_by_xpath(xpath='//button[(@aria-label="See options") or (@aria-label="Optionen anzeigen")]')
+            selenium.click_by_xpath('//button[@data-test="entry-delete"]')
+            selenium.click_by_xpath('//button[(text()="Yes") or (text()="Ja")]')
+        except NoSuchElementException:
+            break
+
+    # Add work experience
+    for work_ex in candidate_data['work_experiences']:
+        selenium.click_by_xpath(xpath='//button[@data-test="add-experience"]')
+
+        # Send job title
+        selenium.send_keys_by_xpath(xpath='//input[@data-test="job-title-autocomplete"]', 
+                                    keys=[work_ex['position']])
+        # Send company name
+        selenium.send_keys_by_xpath(xpath='//input[@data-test="company-autocomplete"]', 
+                                    keys=[work_ex['company'], Keys.ESCAPE])
+        # Send job location
+        selenium.send_keys_by_xpath(xpath='(//input[@class="sr-location-autocomplete element--input element--block"])[2]', 
+                                    keys=[work_ex['location'], Keys.DOWN, Keys.ENTER])
+        # Send job description
+        selenium.send_keys_by_xpath(xpath='//textarea[@data-test="experience-description"]',
+                                    keys=[work_ex['job_description']])
+        
+        start_date = {'month': str(work_ex['start_month']),
+                    'year': str(work_ex['start_year'])}
+        end_date = {'month': str(work_ex['end_month']),
+                    'year': str(work_ex['end_year'])}
+        selenium.add_dates_smartr(category='experience', 
+                                start=start_date,
+                                end=end_date)
+
+        # Save work experience
+        selenium.click_by_xpath(xpath='//button[@data-test="experience-save"]')
+        selenium.sleep(duration=4)
+
+    # Cancel new work experience block
     try:
-        selenium.click_by_xpath(xpath='//button[(@aria-label="See options") or (@aria-label="Optionen anzeigen")]')
-        selenium.click_by_xpath('//button[@data-test="entry-delete"]')
-        selenium.click_by_xpath('//button[(text()="Yes") or (text()="Ja")]')
-    except NoSuchElementException:
-        break
+        selenium.click_by_xpath(xpath='//button[@data-test="experience-cancel"]')
+    except:
+        pass
 
-# Add work experience
-for work_ex in candidate_data['work_experiences']:
-    selenium.click_by_xpath(xpath='//button[@data-test="add-experience"]')
+    for degree in candidate_data['academics']:
+        # Add academics
+        selenium.click_by_xpath(xpath='//button[@data-test="add-education"]')
+        # Univeristy Name
+        selenium.send_keys_by_xpath(xpath='//input[@data-test="institution-autocomplete"]',
+                                    keys=[degree['university'], Keys.DOWN, Keys.ENTER])
+        # University Major
+        selenium.send_keys_by_xpath(xpath='//input[@data-test="education-major"]',
+                                    keys=[degree['major']])
+        # University Degree
+        selenium.send_keys_by_xpath(xpath='//input[@data-test="education-degree"]',
+                                    keys=[degree['degree_level']])
+        try:
+            # University Location
+            selenium.send_keys_by_xpath(xpath='//input[@data-test="location-autocomplete"]',
+                                        keys=[degree['location']])
+        except NoSuchElementException:
+            selenium.send_keys_by_xpath(xpath='//input[(@aria-label="School location") or (@aria-label="Standort der Bildungsstätte")]',
+                                        keys=[degree['location'], Keys.DOWN, Keys.ENTER])
+        # Degree Description
+        selenium.send_keys_by_xpath(xpath='//textarea[@data-test="education-description"]',
+                                    keys=[degree['description']])
+        start_date = {'month': str(degree['start_month']),
+                    'year': str(degree['start_year'])}
+        end_date = {'month': str(degree['end_month']),
+                    'year': str(degree['end_year'])}
+        selenium.add_dates_smartr(category='education', 
+                                start=start_date,
+                                end=end_date)
+        # Save education
+        selenium.click_by_xpath(xpath='//button[@data-test="education-save"]')
 
-    # Send job title
-    selenium.send_keys_by_xpath(xpath='//input[@data-test="job-title-autocomplete"]', 
-                                keys=[work_ex['position']])
-    # Send company name
-    selenium.send_keys_by_xpath(xpath='//input[@data-test="company-autocomplete"]', 
-                                keys=[work_ex['company'], Keys.ESCAPE])
-    # Send job location
-    selenium.send_keys_by_xpath(xpath='(//input[@class="sr-location-autocomplete element--input element--block"])[2]', 
-                                keys=[work_ex['location'], Keys.DOWN, Keys.ENTER])
-
-    selenium.send_keys_by_xpath(xpath='//textarea[@data-test="experience-description"]',
-                                keys=[work_ex['job_description']])
-    
-    start_date = {'month': str(work_ex['start_month']),
-                  'year': str(work_ex['start_year'])}
-    end_date = {'month': str(work_ex['end_month']),
-                'year': str(work_ex['end_year'])}
-    selenium.add_dates_smartr(category='experience', 
-                              start=start_date,
-                              end=end_date)
-
-    # Save work experience
-    selenium.click_by_xpath(xpath='//button[@data-test="experience-save"]')
-
-    selenium.sleep(duration=4)
-
-# Cancel new work experience block
-try:
-    selenium.click_by_xpath(xpath='//button[@data-test="experience-cancel"]')
-except:
-    pass
-
-
-for degree in candidate_data['academics']:
-    # Add academics
-    selenium.click_by_xpath(xpath='//button[@data-test="add-education"]')
-
-    # Univeristy Name
-    selenium.send_keys_by_xpath(xpath='//input[@data-test="institution-autocomplete"]',
-                                keys=[degree['university'], Keys.DOWN, Keys.ENTER])
-    # University Major
-    selenium.send_keys_by_xpath(xpath='//input[@data-test="education-major"]',
-                                keys=[degree['major']])
-    # University Degree
-    selenium.send_keys_by_xpath(xpath='//input[@data-test="education-degree"]',
-                                keys=[degree['degree_level']])
     try:
-        # University Location
-        selenium.send_keys_by_xpath(xpath='//input[@data-test="location-autocomplete"]',
-                                    keys=[degree['location']])
+        # Cancel new education
+        selenium.click_by_xpath(xpath='//button[@data-test="education-cancel"]')
     except NoSuchElementException:
-        selenium.send_keys_by_xpath(xpath='//input[(@aria-label="School location") or (@aria-label="Standort der Bildungsstätte")]',
-                                    keys=[degree['location'], Keys.DOWN, Keys.ENTER])
-        #selenium.send_keys_by_xpath(xpath='//input[@class="sr-location-autocomplete"]',
-        #                            keys=[degree['location'], Keys.DOWN, Keys.ENTER])
-    # Degree Description
-    selenium.send_keys_by_xpath(xpath='//textarea[@data-test="education-description"]',
-                                keys=[degree['description']])
+        pass
 
-    start_date = {'month': str(degree['start_month']),
-                  'year': str(degree['start_year'])}
-    end_date = {'month': str(degree['end_month']),
-                'year': str(degree['end_year'])}
-    selenium.add_dates_smartr(category='education', 
-                              start=start_date,
-                              end=end_date)
+    # Send LI URL
+    selenium.send_keys_by_id(id_='linkedin-input', key=candidate_data['linkedin_url'])
 
-    # Save education
-    selenium.click_by_xpath(xpath='//button[@data-test="education-save"]')
+    # Render Cover Letter 
+    position = selenium.get_text_by_xpath(xpath='//p[@data-test="topbar-job-title"]')
+    position = re.sub(r'\((.*?)\)', '', position).strip()
+    position = position.replace(r'\s+',' ')
 
-try:
-    # Cancel new education
-    selenium.click_by_xpath(xpath='//button[@data-test="education-cancel"]')
-except NoSuchElementException:
-    pass
+    company_name = selenium.attribute_by_xpath(xpath='//img[@class="brand-logo"]', attribute='alt')
+    company_name = company_name.replace(' Logo','').strip()
 
+    job_args = {'COMPANY_NAME': company_name, 
+                'POSITION': position}
+    cover_letter = render_cover_letter(job_args)
 
-# Send LI URL
-selenium.send_keys_by_id(id_='linkedin-input', key=candidate_data['linkedin_url'])
+    selenium.send_keys_by_id(id_='hiring-manager-message-input', key=cover_letter)
 
-# Click on checkbox
-try:
-    selenium.click_by_id(id_='SINGLE')
-except NoSuchElementException:
-    pass
+    # Click on checkbox
+    try:
+        selenium.click_by_id(id_='SINGLE')
+    except NoSuchElementException:
+        pass
 
-# Render Cover Letter 
-position = selenium.get_text_by_xpath(xpath='//p[@data-test="topbar-job-title"]')
-position = re.sub(r'\((.*?)\)', '', position).strip()
-#position = position.replace(r'\(.*?\)','',rege).strip()
-position = position.replace(r'\s+',' ')
-
-company_name = selenium.attribute_by_xpath(xpath='//img[@class="brand-logo"]', attribute='alt')
-company_name = company_name.replace(' Logo','').strip()
-
-job_args = {'COMPANY_NAME': company_name, 
-            'POSITION': position}
-cover_letter = render_cover_letter(job_args)
-
-selenium.send_keys_by_id(id_='hiring-manager-message-input', key=cover_letter)
-
-try:
-    # Click on submit
-    selenium.click_by_xpath(xpath='//button[@data-test="footer-submit"]')
-    notifier.notify_application_success(job_args=job_args, urls=[selenium.url])
-except:
-    notifier.notify_application_failure(job_args=job_args)
+    try:
+        # Click on submit
+        selenium.click_by_xpath(xpath='//button[@data-test="footer-submit"]')
+        notifier.notify_application_success(job_args=job_args, urls=selenium.url)
+    except:
+        notifier.notify_application_failure(job_args=job_args)
